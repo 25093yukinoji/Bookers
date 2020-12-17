84896abd3078f45625364182bf11c1dadaaca740
25093yukinoji <ten.yuki.0601@gmail.com>
2020-12-17 06:25:11 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
-  
+
+  def top
+  end
+
   def index
   end
 
@@ -7,12 +10,20 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.
+    @book = Book.new
   end
-  
+
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
-  
+
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
