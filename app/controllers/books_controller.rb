30211208d543986608c2060d64ee685fd6948b43
25093yukinoji <ten.yuki.0601@gmail.com>
2020-12-19 07:23:59 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,5 @@
 class BooksController < ApplicationController
 
-  def top
-  end
-
   def index
     @books = Book.all
   end
@@ -12,14 +9,14 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.new
+    @book = Book.new(book_params)
   end
 
   def create
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book)
     else
       render("books/new")
     end
@@ -30,9 +27,15 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
+    @book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    book.destroy
+    redirect_to book_path
   end
 
   private
